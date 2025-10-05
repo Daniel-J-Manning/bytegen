6dbcce636d835aa47dd34e1270e793b05736a89d
Daniel-J-Manning <emaildanieljmanning@gmail.com>
2025-10-05 16:42:14 +0100

    feat(main): add GenerateBytes function for random byte generation
    Signed-off-by: Daniel-J-Manning <emaildanieljmanning@gmail.com>

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+/*
+Path: app/cmd
+File: main.go
+Developer(s): Daniel J. Manning
+Created: Sun, 05 Oct 2025
+*/
 
+import (
+	// import packages
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+// Generate Bytes of a length given by the developer
+func GenerateBytes(length int) ([]byte, error) {
+	// Make a variable of b & store the amount length
+	b := make([]byte, length)
+	// If there's an error
+	if _, err := rand.Read(b); err != nil {
+		// Print the error
+		return []byte(""), fmt.Errorf("random generation failed: %w", err)
+	}
+
+	// Ensure it's a []byte and encode it in a base64 string ("")
+	return []byte(base64.StdEncoding.EncodeToString(b)), nil
 }
